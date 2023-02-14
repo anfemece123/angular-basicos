b43107e129be338227427a1dfb838895972d2b7e
anfemece123 <90008499+anfemece123@users.noreply.github.com>
2023-02-14 16:40:04 -0500

    final commit

---
+++ src/app/dbz/main-page/main-page.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+import { Personaje } from '../interfaces/dbz.interfaces';
+
+@Component({
+  selector: 'app-main-page',
+  templateUrl: './main-page.component.html',
+})
+export class MainPageComponent {
+  nuevo: Personaje = {
+    nombre: 'maetro Roshi',
+    poder: 1000,
+  };
+
+  constructor() {}
+}
---
+++ src/app/dbz/personajes/personajes.component.ts
@@ -0,0 +1,14 @@
+import { Component, Input } from '@angular/core';
+import { DbzService } from '../services/dbz.service';
+
+@Component({
+  selector: 'app-personajes',
+  templateUrl: './personajes.component.html',
+})
+export class PersonajesComponent {
+  // @Input() personajes: Personaje[] = [];
+  get personajes() {
+    return this.dbzService.personajes;
+  }
+  constructor(private dbzService: DbzService) {}
+}
